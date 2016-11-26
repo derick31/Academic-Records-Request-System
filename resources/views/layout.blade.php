0e9486fb1535b07f0208e37cdf82f48f51af8ddd
johneim <jsmission@up.edu.ph>
2016-11-27 01:20:46 +0800

    Add files via upload

---
+++ resources/views/layout.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <link href="css/bootstrap.min.css"  rel="stylesheet" type="text/css"/>
+        <link href="css/dashboard.css" rel="stylesheet" type="text/css"/>
+        <script src="js/jquery-3.1.1.min.js"></script>
+        <script src="js/bootstrap.min.js"></script>
+
+        {{-- ALUMNI CSS AND JAVASCRIPT --}}
+        <link href="css/alumni/alumni.css"  rel="stylesheet" type="text/css"/>
+        <script src="js/alumni/alumni.js"></script>
+
+        <title>@yield('title')</title>
+    </head>
+    <body>
+	    @include('navbar')
+	    @yield('content')
+    </body>
+</html>
