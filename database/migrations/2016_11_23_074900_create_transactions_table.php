c7ad7bb5c63f160c8489d294ff979fd229d05ac6
johneim <jsmission@up.edu.ph>
2016-12-08 04:20:23 +0800

    Update 2016_11_23_074900_create_transactions_table.php

--- database/migrations/2016_11_23_074900_create_transactions_table.php
+++ database/migrations/2016_11_23_074900_create_transactions_table.php
@@ -15,16 +15,16 @@ class CreateTransactionsTable extends Migration
     {
         Schema::create('transactions', function (Blueprint $table) {
             $table->increments('transaction_id');
-            $table->integer('alummnus_id')->unsigned()->index();
-            // $table->foreign('alummnus_id')->references('alumnus_id')->on('alumni')->onDelete('cascade');
-            $table->integer('staff_id')->unsigned()->index();
-            // $table->foreign('staff_id')->references('staff_id')->on('staff')->onDelete('cascade');
-            $table->integer('purpose_id')->unsigned()->index();
+            $table->integer('alumnus_id')->unsigned()->index();
+            $table->foreign('alumnus_id')->references('alumnus_id')->on('alumni')->onDelete('cascade');
+            $table->integer('staff_id')->unsigned()->index()->nullable();
+            $table->foreign('staff_id')->references('staff_id')->on('staff')->onDelete('cascade');
+            $table->integer('purpose_id')->unsigned()->index()->nullable();
             $table->enum('retrieval_type',[0,1]);
-            $table->enum('status',['pending','paid','ready for pickup','mailed','received']);
-            $table->string('mailing_adress');
-            $table->string('claim_code');
-            $table->string('receipt_number');
+            $table->enum('status',['pending','paid','ready for pickup','mailed','received'])->nullable();
+            // $table->string('mailing_adress');
+            $table->string('claim_code')->nullable();
+            $table->string('receipt_number')->nullable();
             $table->timestamps();
         });
     }
