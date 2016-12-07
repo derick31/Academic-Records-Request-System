ff5e37a64d9b729bd4a38b9f2532c7a8e81f8c91
johneim <jsmission@up.edu.ph>
2016-12-08 00:18:26 +0800

    Update User.php
    added alumnus relationship to alumni table

--- app/User.php
+++ app/User.php
@@ -26,4 +26,8 @@ class User extends Authenticatable
     protected $hidden = [
         'password', 'remember_token',
     ];
+    public function alumnus ()
+    {
+        return $this->hasOne(Alumnus::class);
+    }
 }
