5a5aa1e16088b0819aca72115efe5fc491e07de3
johneim <jsmission@up.edu.ph>
2016-12-08 04:19:56 +0800

    Update 2016_11_23_073950_create_alumni_table.php

--- database/migrations/2016_11_23_073950_create_alumni_table.php
+++ database/migrations/2016_11_23_073950_create_alumni_table.php
@@ -15,7 +15,7 @@ class CreateAlumniTable extends Migration
     {
         Schema::create('alumni', function (Blueprint $table) {
             $table->increments('alumnus_id');
-            $table->string('alumnus_password');
+            // $table->string('alumnus_password');
             $table->string('first_name');
             $table->string('last_name');
             $table->string('middle_name');
@@ -26,6 +26,10 @@ class CreateAlumniTable extends Migration
             $table->string('birthplace');
             $table->string('father_name');
             $table->string('mother_name');
+            $table->integer('degree_id')->unsigned()->index();
+            $table->foreign('degree_id')->references('degree_id')->on('degrees')->onDelete('cascade');
+            $table->integer('year_started');
+            $table->integer('year_ended');
             $table->integer('apply_status');    //added nov 26
             $table->date('apply_date');         //added nov 26
             $table->timestamps();
