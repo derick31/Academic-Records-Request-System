97904a32a94aaa4ffad3b22d90f145ae95c6fc14
johneim <jsmission@up.edu.ph>
2016-12-08 01:08:53 +0800

    Update sidebar.blade.php
    update

--- resources/views/sidebar.blade.php
+++ resources/views/sidebar.blade.php
@@ -1,17 +1,27 @@
-@if(Auth::user()->type=='alumni')
+@if(Auth::user()->type=='alumni' AND $sidebar_active!='none')
 <div class="jumbotron col-md-3 sidebar-column" style="margin-left: -15px; height: 633px;">
             <div style="">
                 <img class="prof-img" src="../img/UP.png" style="border-radius: 100%;"/>
                 <span class="welcome-note-dashboard">Welcome,</span>
                 <span class="welcome-name"> [name]</span> 
             </div>
-            <div class="active-clicked" id="dashboard">
+
+            <div
+            @if($sidebar_active == 'dashboard')
+                class="active-clicked"
+            @endif class="account-sidebar">
                 <a href="{{ url('/dashboard') }}">Dashboard</a>
             </div>
-            <div class="account-sidebar" id="editprofile">
-               <a href="{{ url('/editprofile') }}">Edit Profile</a>
+            <div 
+            @if($sidebar_active == 'editaccount')
+                class="active-clicked"
+            @endif class="account-sidebar" id="editprofile">
+               <a href="{{ url('/editaccount') }}">Edit Profile</a>
             </div>
-            <div class="account-sidebar" id="requests">
+            <div 
+            @if($sidebar_active == 'requestrecord')
+                class="active-clicked"
+            @endif class="account-sidebar" id="requests">
                <a href="{{ url('/requestrecord') }}">Request Records</a>
             </div>
             <div class="account-sidebar" id="logout">
